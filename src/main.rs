c13bafd08a6d43249aa7f17d772c0f9c3ab8bfe5
mayukh-chr <mayukhsaha.private@gmail.com>
2024-01-17 19:21:01 +0530

    include instructions on how to run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//export PREFIX="$HOME/opt/cross"
+//export TARGET=i686-elf
+//export PATH="$PREFIX/bin:$PATH"
+//cargo build followed by > qemu-system-i386 -kernel target/target/debug/kernel
+
 #![allow(bad_asm_style)]
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
@@ -10,7 +15,7 @@ use core::panic::PanicInfo;
 // naked function + some inline asm, but this seems much more straight forward.
 global_asm!(include_str!("boot.s"));
 
-#[no_mangle]
+#[no_mangle]//memset: iterates over an array of chars.
 pub unsafe extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
     let c = c as u8;
     for i in 0..n {
